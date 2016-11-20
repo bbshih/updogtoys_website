f0fb0bda22f3c8a15c768affa00fc04a3c771734
billyshih <me@billyshih.com>
2016-11-20 02:29:07 -0800

    Add color, change nav
    Add color, change nav

--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
 
 /***************** Waypoints ******************/
   $('.wp1').waypoint(function() {
-    $('.wp1').addClass('animated fadeInLeft');
+    $('.wp1').addClass('animated fadeInUp');
   }, {
     offset: '75%'
   });
   $('.wp2').waypoint(function() {
-    $('.wp2').addClass('animated fadeInUp');
+    $('.wp2').addClass('animated fadeInDown');
   }, {
     offset: '75%'
   });
   $('.wp3').waypoint(function() {
     $('.wp3').addClass('animated fadeInDown');
   }, {
-    offset: '55%'
+    offset: '75%'
   });
   $('.wp4').waypoint(function() {
     $('.wp4').addClass('animated fadeInDown');
@@ -23,12 +23,44 @@ $(document).ready(function() {
     offset: '75%'
   });
   $('.wp5').waypoint(function() {
-    $('.wp5').addClass('animated fadeInUp');
+    $('.wp5').addClass('animated fadeInDown');
   }, {
     offset: '75%'
   });
   $('.wp6').waypoint(function() {
     $('.wp6').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+  $('.wp7').waypoint(function() {
+    $('.wp7').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+  $('.wp8').waypoint(function() {
+    $('.wp8').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+  $('.wp9').waypoint(function() {
+    $('.wp9').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+  $('.wp10').waypoint(function() {
+    $('.wp10').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+
+  $('.wpinstagram').waypoint(function() {
+    $('.wpinstagram').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
+
+  $('.wpfooter').waypoint(function() {
+    $('.wpfooter').addClass('animated fadeInUp');
   }, {
     offset: '75%'
   });
