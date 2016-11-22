a82ac83a09f13118c3e18fe2472950c8346b7a88
billyshih <me@billyshih.com>
2016-11-21 21:50:30 -0800

    change featured products

--- src/js/app.js
+++ src/js/app.js
@@ -5,64 +5,64 @@ $(document).ready(function() {
   $('.wp1').waypoint(function() {
     $('.wp1').addClass('animated fadeInUp');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp2').waypoint(function() {
     $('.wp2').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp3').waypoint(function() {
     $('.wp3').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp4').waypoint(function() {
     $('.wp4').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp5').waypoint(function() {
     $('.wp5').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp6').waypoint(function() {
     $('.wp6').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp7').waypoint(function() {
     $('.wp7').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp8').waypoint(function() {
     $('.wp8').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp9').waypoint(function() {
     $('.wp9').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
   $('.wp10').waypoint(function() {
     $('.wp10').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
 
   $('.wpinstagram').waypoint(function() {
     $('.wpinstagram').addClass('animated fadeInDown');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
 
   $('.wpfooter').waypoint(function() {
     $('.wpfooter').addClass('animated fadeInUp');
   }, {
-    offset: '75%'
+    offset: '90%'
   });
 
   $(".fancybox").fancybox({
@@ -71,6 +71,11 @@ $(document).ready(function() {
 
   $('.fancybox-media').fancybox({
     padding: 0,
+    maxWidth: 10000,
+    maxHeight: 10000,
+    minWidth: '70%',
+    minWidth: '70%',
+    aspectRatio: true,
     helpers : {
       media: {
           youtube : {
